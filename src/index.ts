ebbd075c9b22d1fb99ceff68d01ee3f31e86f382
Louis Beullens <louis@beullens.com>
2025-10-23 09:16:06 +0200

    fixing / route

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,12 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __dirname = path.join(path.dirname(__filename), '../')
 
 const app = express()
 
 app.use(express.json())
 app.use("/", (req, res) => {
-  // Adjust the path to your actual file location
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.use("*", express.static("public"))
